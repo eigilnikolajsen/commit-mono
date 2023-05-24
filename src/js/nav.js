24dc53f109fb8f52dd03f2f4ea9f74719f1bca81
Eigil Nikolajsen <eigi0088@gmail.com>
2023-05-24 14:25:16 +0200

    playing with focus

--- src/js/nav.js
+++ src/js/nav.js
@@ -13,38 +13,57 @@ websiteData.sections.forEach((section, index) => {
 })
 
 let currentSection = 1
+let insideTextField = false
 
 window.addEventListener("keydown", (e) => {
-	if (currentSection == 1) {
-		const activeKey = document.querySelector(`.key[data-key-code="${e.code}"]`)
-		activeKey?.classList.add("active_key")
+	// escape textfield
+	if (e.code == "Escape") {
+		insideTextField = false
+		active.setAttribute("contenteditable", "false")
+	}
+	// edit textfield
+	if (e.code == "KeyE") {
+		insideTextField = true
+		active.setAttribute("contenteditable", "true")
 	}
 
-	if (
-		e.code == "KeyW" ||
-		e.code == "KeyA" ||
-		e.code == "KeyS" ||
-		e.code == "KeyD" ||
-		e.code == "KeyR" ||
-		e.code == "Minus" ||
-		e.code == "Slash"
-	) {
-		main.style.opacity = 0.02
-		pushPage(e.code)
+	if (!insideTextField) {
+		// show keypress on frontpage
+		if (currentSection == 1) {
+			const activeKey = document.querySelector(`.key[data-key-code="${e.code}"]`)
+			activeKey?.classList.add("active_key")
+		}
+
+		// push page
+		if (
+			e.code == "KeyW" ||
+			e.code == "KeyA" ||
+			e.code == "KeyS" ||
+			e.code == "KeyD" ||
+			e.code == "KeyR" ||
+			e.code == "Minus" ||
+			e.code == "Slash"
+		) {
+			main.style.opacity = 0.04
+			pushPage(e.code)
+		}
 	}
 })
 
 const main = document.querySelector("main")
 const mainScale = document.querySelector("#main_scale")
 const rem = getComputedStyle(document.documentElement).fontSize.split("px")[0]
+
 window.addEventListener("keyup", (e) => {
-	console.log(`keyup: ${e.code}`)
-	if (currentSection == 1) {
-		const activeKey = document.querySelector(`.key[data-key-code="${e.code}"]`)
-		activeKey?.classList.remove("active_key")
-	}
-	if (e.code.includes("Digit")) {
-		sectionNavigation(e.code.split("Digit")[1])
+	if (!insideTextField) {
+		console.log(`keyup: ${e.code}`)
+		if (currentSection == 1) {
+			const activeKey = document.querySelector(`.key[data-key-code="${e.code}"]`)
+			activeKey?.classList.remove("active_key")
+		}
+		if (e.code.includes("Digit")) {
+			sectionNavigation(e.code.split("Digit")[1])
+		}
 	}
 	main.style.opacity = 1
 })
@@ -102,13 +121,38 @@ function pushPage(keyCode) {
 	}
 }
 
-let active
+let active // saves what DOM element is currently active
+let focusTimeOutID // to be able to use clearTimeout()
 document.addEventListener("focusin", (e) => {
+	// save current focused element
 	active = document.activeElement
+
+	// when current focused element is blurred, start a timer of 100ms.
+	active.addEventListener("blur", onBlurIn)
+
+	// clear timeout when a new element is focused
+	clearTimeout(focusTimeOutID)
+	focusTimeOutID = null
+
+	// navigation
 	if (active.classList.contains("nav_element")) {
 		sectionNavigation(active.dataset.sectionIndex)
 	}
+
+	console.log(active, active.getAttribute("contenteditable"))
+	if (active.dataset.edit === "true") {
+		active.setAttribute("contenteditable", "true")
+		insideTextField = true
+	}
 })
+
+function onBlurIn(e) {
+	// remove event listener from so they don't stack
+	e.target.removeEventListener("blur", onBlurIn)
+
+	// if this timer runs out before a new element is focused, refocus same element
+	focusTimeOutID = setTimeout(() => active.focus(), 100)
+}
 
 function sectionNavigation(num) {
 	const activeSection = document.querySelector(`#section_${num}`)
